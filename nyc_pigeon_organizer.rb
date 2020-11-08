aca2492e295ab9bd4cd14ecc84ddf4e4620791ce
Charlie Knight <williamchknight@gmail.com>
2020-11-08 20:10:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final|
+    value.each do |ik, names|
+      names.each do |name|
+        #binding.pry
+        if !final[name] #makes it so it doesn't add a name twice
+          final[name] = {}  
+        end
+        if !final[name][key]
+          final[name][key] = []
+        end
+        final[name][key].push(ik.to_s)
+      end
+    end   
+  end
 end
