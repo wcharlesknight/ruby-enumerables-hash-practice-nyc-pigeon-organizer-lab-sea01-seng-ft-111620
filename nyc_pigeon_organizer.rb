536f9bec97978ed9bc0bfcb693415de907289493
Charlie Knight <williamchknight@gmail.com>
2020-11-11 03:19:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final|
     value.each do |ik, names|
       names.each do |name|
-        #binding.pry
         if !final[name] #makes it so it doesn't add a name twice
           final[name] = {}  
         end
@@ -13,6 +12,7 @@ def nyc_pigeon_organizer(data)
         end
         final[name][key].push(ik.to_s)
       end
-    end   
+    end
+  binding.pry  
   end
 end
